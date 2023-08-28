fa992d756802c0e7c577a0b3ebdebc1c79338d4b
Jack McDowall <jmcdowall2@huskers.unl.edu>
2023-08-27 21:38:03 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,21 @@
+/**
+ * Author: Alec Whitten
+ * Co-Auther: Jack McDowall
+ * Date: 2023/08/24
+ *
+ * pring name and
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Alec Whitten!!\n");
+  printf("Computer Science!\n\n\n");
+
+  printf("Jack McDowall!!\n");
+  printf("Electrical Engineering!\n");
+
+  return 0;
+}
